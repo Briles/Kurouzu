c782aa8658663a0e7720deded2d95ee1d2d19dcf
Brian <brian@dougreilly.com>
2015-07-08 02:07:24 -0400

    Fixed build & readme

--- build.ps1
+++ build.ps1
@@ -1,28 +1,28 @@
-$cwd = "C:\Users\Brian\Business\Projects\Kurouzu\"
+$cwd = "./"
 
-if (Test-Path("$cwd\Source\logs")) {
-    Remove-Item -Recurse -Path "$cwd\Source\logs" -Force
+if (Test-Path("$cwd\logs")) {
+    Remove-Item -Recurse -Path "$cwd\logs" -Force
 }
-if (Test-Path("$cwd\Source\Assets")) {
-    Remove-Item -Recurse -Path "$cwd\Source\Assets" -Force
+if (Test-Path("$cwd\Assets")) {
+    Remove-Item -Recurse -Path "$cwd\Assets" -Force
 }
 $csvs = @();
-Get-ChildItem "$cwd\Source\data\*\*\" -Recurse -Filter *.csv | % {
+Get-ChildItem "$cwd\data\*\*\" -Recurse -Filter *.csv | % {
     $game = $_.Directory.Name;
     $self = $_.Name;
-    Copy-Item -Path $_.FullName -Destination "$cwd\Source\data\$game $self" -Force;
+    Copy-Item -Path $_.FullName -Destination "$cwd\data\$game $self" -Force;
     $csvs += "/res:data\$game $self";
 }
-          # /win32res:"$cwd\gga\GGa\Release\x64\GGa.res" `
+
 $build = (&"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe" `
-          /win32icon:"..\Graphics\icon.ico" `
+          /win32icon:"Graphics\icon.ico" `
           $csvs `
           /optimize `
           /platform:x64 `
           /target:exe `
           /out:kurouzu.exe `
-          bin\*.cs
+          src\*.cs
 )
 if ($build.Count -gt 5){ $build[5..$build.Count] | foreach { Write-Host $_ -ForegroundColor Red; } } else { clear }
 
-Get-ChildItem -File "$cwd\Source\data\" -Filter *.csv | Remove-Item -Force
+Get-ChildItem -File "$cwd\data\" -Filter *.csv | Remove-Item -Force
